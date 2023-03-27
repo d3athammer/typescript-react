948f90a0b82bd7d40e5d1101a62617a91afcf7ac
ting wei yap <yap.tingwei@gmail.com>
2023-03-27 17:27:53 +0800

    adding typescript interface:

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,11 @@
 import { FormEvent, useRef, useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 
+interface FormData {
+  name: string,
+  age: number
+}
+
 const Form = () => {
 
   // 1st way to get input field from a form
@@ -32,7 +37,7 @@ const Form = () => {
   // }
 
   // 3rd way to get input field from a dom ( gotta change the code inside return)
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
   const onSubmit = ( data: FieldValues) => {
     console.log(data);
@@ -51,7 +56,7 @@ const Form = () => {
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
         <input { ...register('age', { required: true })} id="age" type="number" className="form-control" />
-        { errors.name?.type === 'required' && <p className='text-danger'>Age is required</p> }
+        { errors.age?.type === 'required' && <p className='text-danger'>Age is required</p> }
 
       </div>
       <button className="btn btn-primary">Submit</button>
