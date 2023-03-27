309de4c4d5cf480adc4ea9bf674d851aa2a30600
ting wei yap <yap.tingwei@gmail.com>
2023-03-27 18:18:52 +0800

    completed validation with zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,12 @@
 import { FormEvent, useRef, useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const schema = z.object({
-  name: z.string().min(3),
-  age: z.number().min(8)
+  // chaining the validations and applying custom messages to each of the validations
+  name: z.string().min(3, { message: 'name must be at least 3 characters'}),
+  age: z.number( {invalid_type_error: 'Age field is required'}).min(18, { message: 'Age must be above 18 '})
 });
 
 type FormData = z.infer<typeof schema>;
@@ -40,7 +42,9 @@ const Form = () => {
   // }
 
   // 3rd way to get input field from a dom ( gotta change the code inside return)
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  // 4th method is to use Zod for validation
+  //with zodResolver, we can set resolver to zoldresolver.
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = ( data: FieldValues) => {
     console.log(data);
@@ -50,16 +54,16 @@ const Form = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input { ...register('name', { required: true, minLength: 3 })} id="name" type="text" className="form-control" />
+        <input {...register('name')} id="name" type="text" className="form-control" />
         {/* the question mark beside name is called optional chaining, to prevent runtime error if no name is inout */}
-        { errors.name?.type === 'required' && <p className='text-danger'>Name is required</p> }
-        { errors.name?.type === 'minLength' && <p className='text-danger'>Name must have at least 3 characters</p> }
-
+        {/* There is no need to check for type because zod already does it for you */}
+        { errors.name && <p className='text-danger'>{errors.name.message}</p> }
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input { ...register('age', { required: true })} id="age" type="number" className="form-control" />
-        { errors.age?.type === 'required' && <p className='text-danger'>Age is required</p> }
+        {/* We set age to accept only numbers else we'll throw an error */}
+        <input {...register('age', { valueAsNumber: true })} id="age" type="number" className="form-control" />
+        { errors.age && <p className='text-danger'>{errors.age.message}</p> }
 
       </div>
       <button className="btn btn-primary">Submit</button>
