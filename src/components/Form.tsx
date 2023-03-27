609f42ae22c520076c54dd09bf3fb29b98c02865
ting wei yap <yap.tingwei@gmail.com>
2023-03-27 17:25:38 +0800

    adding validation using react form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useRef, useState } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
 
 const Form = () => {
 
@@ -17,28 +18,41 @@ const Form = () => {
   //   console.log(person);
   // }
 
-  // 2nd way to get input field from a dom
-  const [ person, setPerson ] = useState({
-    name: '',
-    age: ''
-  })
-  const handleSubmit = (event : FormEvent) => {
-    event.preventDefault();
-    console.log(person);
 
+
+  // 2nd way to get input field from a dom
+  // const [ person, setPerson ] = useState({
+  //   name: '',
+  //   age: ''
+  // })
+  // const handleSubmit = (event : FormEvent) => {
+  //   event.preventDefault();
+  //   console.log(person);
+
+  // }
+
+  // 3rd way to get input field from a dom ( gotta change the code inside return)
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = ( data: FieldValues) => {
+    console.log(data);
   }
 
-
   return (
-
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input value={person.name} onChange={(event) => setPerson({...person, name: event.target.value}) } id="name" type="text" className="form-control" />
+        <input { ...register('name', { required: true, minLength: 3 })} id="name" type="text" className="form-control" />
+        {/* the question mark beside name is called optional chaining, to prevent runtime error if no name is inout */}
+        { errors.name?.type === 'required' && <p className='text-danger'>Name is required</p> }
+        { errors.name?.type === 'minLength' && <p className='text-danger'>Name must have at least 3 characters</p> }
+
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input value={person.age} onChange={(event) => setPerson({...person, age: parseInt(event.target.value)}) } id="age" type="number" className="form-control" />
+        <input { ...register('age', { required: true })} id="age" type="number" className="form-control" />
+        { errors.name?.type === 'required' && <p className='text-danger'>Age is required</p> }
+
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
